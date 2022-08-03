d9ae57cd6c37e2627de9dd1f0b9c30381136c1de
KienJerry <kienntpk01531@gmail.com>
2022-08-03 23:50:46 +0700

    push_bootstrap

---
+++ [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Bootstrap/validate.js
@@ -0,0 +1,29 @@
+$(function() {
+$("form[name='login']").validate({
+	rules: {
+	
+	email: {
+		required: true,
+		email: true
+	},
+	password: {
+		required: true,
+		
+	}
+	},
+	messages: {
+	email: "Please enter a valid email address",
+	
+	password: {
+		required: "Please enter password",
+	
+	}
+	
+	},
+	submitHandler: function(form) {
+	alert("ok")
+	}
+});
+});
+			 
+	
