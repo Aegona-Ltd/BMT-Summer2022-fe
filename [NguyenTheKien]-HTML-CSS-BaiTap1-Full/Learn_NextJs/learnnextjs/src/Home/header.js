b7664b0f5b7142995cc4bb443896e23f279b64f6
KienJerry <kienntpk01531@gmail.com>
2022-08-18 11:59:34 +0700

    push_icon_antd

--- [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/header.js
+++ [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/header.js
@@ -1,14 +1,56 @@
 import 'antd/dist/antd.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from '../../styles/home.module.scss';
-import { Button } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Tooltip, Row, Col, Button } from 'antd';
+import { UserOutlined, GooglePlusOutlined, PhoneOutlined, FieldTimeOutlined } from "@ant-design/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import Link from 'next/dist/client/link';
 export default function HeaderHome() {
   return (
     <div className={styles.header_full}>
       {/*  */}
-      <div className={styles.header_top}>a</div>
+      <div className={styles.header_top}>
+        <Row>
+          <Col md={12}>
+          <div className={styles.header_icon_top}>
+                <Tooltip
+                  title="Google+"
+                  placement="bottom"
+                  color="#db4a39"
+                  key="#db4a39"
+                >
+                  <div className="social linkedin" style={{ width: '23px', height: '23px' }} >
+                    <GooglePlusOutlined />
+                  </div>
+                </Tooltip>
+
+                <Tooltip
+                  title="Facebook"
+                  placement="bottom"
+                  color="#4267B2"
+                  key="#4267B2"
+                >
+                  <div className="social linkedin" style={{ width: '23px', height: '23px' }}>
+                    <FontAwesomeIcon icon={faFacebookF} className={styles.icon1} />
+                  </div>
+                </Tooltip>
+
+                <Tooltip
+                  title="linkedin"
+                  placement="bottom"
+                  color="#0e76a8"
+                  key="#0e76a8"
+                >
+                  <div className="social linkedin" style={{ width: '23px', height: '23px' }}>
+                    <FontAwesomeIcon icon={faLinkedinIn} className={styles.icon1} />
+                  </div>
+                </Tooltip>
+              </div>
+          </Col>
+          <Col md={12}>abc</Col>
+        </Row>
+      </div>
 
       {/*  */}
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
