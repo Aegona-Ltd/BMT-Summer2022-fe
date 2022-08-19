902166af332e3d54d390888ca19d4159f352d131
KienJerry <kienntpk01531@gmail.com>
2022-08-19 10:16:26 +0700

    push_code_responsive_header

--- [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/header.js
+++ [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/header.js
@@ -2,19 +2,17 @@ import 'antd/dist/antd.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import styles from '../../styles/home.module.scss';
-import { Row, Col, Button } from 'antd';
-import { UserOutlined, GooglePlusOutlined} from "@ant-design/icons";
+import { Row, Col } from 'antd';
+import { GooglePlusOutlined } from "@ant-design/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faLinkedinIn, faPhoenixSquadron } from '@fortawesome/free-brands-svg-icons';
-import Link from 'next/dist/client/link';
+import { faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 export default function HeaderHome() {
   return (
     <div className={styles.header_full}>
-      {/*  */}
+      {/* header_top */}
       <div className={styles.header_top}>
         <Row>
           <Col md={9} lg={15} sm={5}>
-          {/* <Col md={15}> */}
             <div className={styles.header_icon_top}>
               <div className={styles.header_icon_top_bg}>
                 <GooglePlusOutlined />
@@ -28,33 +26,27 @@ export default function HeaderHome() {
             </div>
           </Col>
           <Col md={15} lg={9} sm={19}>
-          {/* <Col md={9}> */}
             <div className={styles.header_icon_right}>
               <Col md={8} sm={24}>
-              {/* <Col md={8}> */}
-              <div className={styles.header_icon_right_phone}>
-              <i class="bi bi-telephone"/>
-                <span className={styles.header_icon_right_text_right}>Hotline<span>1900 2021</span></span>
-              </div>
+                <div className={styles.header_icon_right_phone}>
+                  <i className="bi bi-telephone" />
+                  <span className={styles.header_icon_right_text_right}>Hotline<span>1900 2021</span></span>
+                </div>
               </Col>
               <Col md={16} sm={24}>
-              {/* <Col md={16}> */}
-              <div className={styles.header_icon_right_phone}>
-              <i class="bi bi-alarm"></i>
-                <span className={styles.header_icon_right_text_right}>Giờ Làm Việc<span>Thứ 2 - Thứ 6 : 08:00 - 17:00</span></span>
-              </div>
+                <div className={styles.header_icon_right_phone}>
+                  <i className="bi bi-alarm"></i>
+                  <span className={styles.header_icon_right_text_right}>Giờ Làm Việc<span>Thứ 2 - Thứ 6 : 08:00 - 17:00</span></span>
+                </div>
               </Col>
             </div>
           </Col>
         </Row>
       </div>
-
-      {/*  */}
+      {/* header_navbar */}
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
         <div className="container-fluid">
-          <Link href="/">
-            <a className="navbar-brand" style={{ fontFamily: 'Montserrat' }}><div style={{ fontFamily: 'Montserrat' }}>AEGO<span>NA</span></div></a>
-          </Link>
+            <a href='/' className="navbar-brand" style={{ fontFamily: 'Montserrat' }}><div style={{ fontFamily: 'Montserrat' }}>AEGO<span>NA</span></div></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -73,11 +65,10 @@ export default function HeaderHome() {
                 <a className="nav-link" href="#">LIÊN HỆ</a>
               </li>
             </ul>
-
-            <button type="button" class="btn btn-warning">
-            <i class="bi bi-person-circle"></i> Đăng Nhập
-            </button>
           </div>
+          <a href='/login' type="button" className="btn btn-warning">
+            <i className="bi bi-person-circle"></i> Đăng Nhập
+          </a>
         </div>
       </nav>
     </div>
--- [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/main.js
+++ [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/main.js
@@ -1,10 +1,44 @@
-import styles from '../../styles/Home.module.css';
-export default function Main() {
+import 'antd/dist/antd.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import styles from '../../styles/home.module.scss';
+import { Row, Col } from 'antd';
+import { GooglePlusOutlined } from "@ant-design/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/dist/client/link';
+export default function MainHome() {
   return (
-    <div className={styles.container}>
-        <h1 className={styles.title}>
-          Main
-        </h1>
+    <div className={styles.main_full}>
+      <div className={styles.main_top}>
+        <Row>
+          <Col lg={12} md={24} xs={24}>
+            <div className={styles.main_top_text_left_bg}>
+              <div className={styles.main_top_text_left_dot}></div>
+              <div className={styles.main_top_text_left_text}>Giới Thiệu Chung
+                <div className={styles.main_top_text_left_text_v1}>Website học tiếng anh</div>
+              </div>
+              <div className={styles.main_top_text_left_text1}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </div>
+              <div className={styles.main_top_text_left_btn}>
+                <button type="button" className="btn btn-warning">Đăng Ký Học Ngay</button>
+                <a type="button" className="btn btn-outline-warning">Dùng Thử Miễn Phí</a>
+              </div>
+            </div>
+          </Col>
+          <Col lg={12} md={24} xs={24}>
+            <div className={styles.main_top_text_right_bg}>
+              <div className={styles.main_top_text_right_bg_color}>
+              <div className={styles.test}>sâd</div>
+              <div>sâd</div>
+              <div>sâd</div>
+              </div>
+
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
