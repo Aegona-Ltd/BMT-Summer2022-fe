c9ec29cfd56f380f9a47813ef63a3ca445917f05
KienJerry <kienntpk01531@gmail.com>
2022-08-22 17:44:17 +0700

    complete_landing_responsive_desktop_mobile_tablet

--- [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/Arr_Home/arr_footer.js
+++ [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/Arr_Home/arr_footer.js
@@ -1,76 +1,110 @@
 import styles from '../../../styles/home.module.scss';
+import { Input } from 'antd';
 
 const link_arr_1 = [
     {
-        id : 1,
-        class : "bi bi-envelope",
-        name : 'Email',
-        span : 'contact@aegona.com'
+        id: 1,
+        class: "bi bi-envelope",
+        name: 'Email',
+        span: 'contact@aegona.com'
     },
     {
-        id : 2,
-        class : "bi bi-telephone",
-        name : 'Hotline',
-        span : '1900 2021 '
+        id: 2,
+        class: "bi bi-telephone",
+        name: 'Hotline',
+        span: '1900 2021 '
     },
     {
-        id : 3,
-        class : "bi bi-alarm",
-        name : 'Giờ làm việc',
-        span : 'Thứ 2 - Thứ 6 : 08:00 - 17:00'
+        id: 3,
+        class: "bi bi-alarm",
+        name: 'Giờ làm việc',
+        span: 'Thứ 2 - Thứ 6 : 08:00 - 17:00'
     },
 ]
 
 const link_arr_2 = [
     {
-        name : 'AEGO',
-        name2 :'NA',
-        class_r1 : styles.footer_center_text,
-        class_r2 : '',
-        dulieu : [
+        name: 'AEGO',
+        name2: 'NA',
+        class_r1: styles.footer_center_text,
+        class_r2: '',
+        dulieu: [
             {
-                icon : "bi bi-telephone-fill",
-                title : '028 7109 2939',
-            }
+                icon: "bi bi-telephone-fill",
+                title: '028 7109 2939',
+            },
+            {
+                icon: "bi bi-envelope-fill",
+                title: 'contact@aegona.com',
+            },
+            {
+                icon: "bi bi-geo-alt-fill",
+                title: 'Lầu 4, Tòa nhà QTSC9 công viên phần mềm Quang Trung, Quận 12, Tp.HCM',
+            },
         ]
     },
     {
-        name : 'ABC',
-        name2 :'',
-        class_r1 : styles.footer_center_text_1,
-        class_r2 : styles.footer_center_border,
-        dulieu : [
+        name: 'ABC',
+        name2: '',
+        class_r1: styles.footer_center_text_1,
+        class_r2: styles.footer_center_border,
+        dulieu: [
             {
-                icon : "bi bi-dot",
-                title : 'Thông tin học thuật',
-            }
+                icon: "bi bi-dot",
+                title: 'Thông tin học thuật',
+            },
+            {
+                icon: "bi bi-dot",
+                title: 'Hướng dẫn khóa học',
+            },
+            {
+                icon: "bi bi-dot",
+                title: 'Hướng dẫn đăng ký',
+            },
+            {
+                icon: "bi bi-dot",
+                title: 'Hướng dẫn thanh toán',
+            },
         ]
     },
     {
-        name : 'CDF',
-        name2 :'',
-        class_r1 : styles.footer_center_text_1,
-        class_r2 : styles.footer_center_border,
-        dulieu : [
+        name: 'CDF',
+        name2: '',
+        class_r1: styles.footer_center_text_1,
+        class_r2: styles.footer_center_border,
+        dulieu: [
             {
-                icon : "bi bi-dot",
-                title : 'Trợ giúp',
-            }
+                icon: "bi bi-dot",
+                title: 'Trợ giúp',
+            },
+            {
+                icon: "bi bi-dot",
+                title: 'Bản quyền',
+            },
+            {
+                icon: "bi bi-dot",
+                title: 'Liên hệ',
+            },
         ]
     },
     {
-        name : 'Liên Hệ Tư Vấn',
-        name2 :'',
-        class_r1 : styles.footer_center_text_1,
-        class_r2 : styles.footer_center_border,
-        dulieu : [
+        name: 'Liên Hệ Tư Vấn',
+        name2: '',
+        class_r1: styles.footer_center_text_1,
+        class_r2: styles.footer_center_border,
+        dulieu: [
             {
-                id: 1
-            }
+                icon: "",
+                title: <Input placeholder="Email của bạn" />
+            },
+            {
+                icon: "",
+                title: <button type="button" className="btn btn-warning">Gửi Email</button>
+            },
         ]
     },
 ]
 
 export {
-    link_arr_1 , link_arr_2
+    link_arr_1, link_arr_2
 }
--- [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/footer.js
+++ [NguyenTheKien]-HTML-CSS-BaiTap1-Full/Learn_NextJs/learnnextjs/src/Home/footer.js
@@ -1,6 +1,8 @@
 import styles from '../../styles/home.module.scss';
 import { Row, Col } from 'antd';
 import { link_arr_1, link_arr_2 } from './Arr_Home/arr_footer';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 export default function FooterHome() {
   return (
     <div className={styles.footer_full}>
@@ -25,18 +27,41 @@ export default function FooterHome() {
               <Col lg={6} md={11} xs={24} className={styles.footer_center} key={key}>
                 <div className={index.class_r1}>{index.name}<span>{index.name2}</span></div>
                 <div className={index.class_r2}></div>
-                {index.dulieu.map((a , b) => {
-                  return(
-                    <Col lg={6} md={11} xs={24} key={b} className={styles.footer_center_children}>
-                      <div className={styles.footer_center_icon_children}>
-                        <i className={a.icon}></i> <span>{a.title}</span>
-                      </div>
-                    </Col>
-                  )
-                })}
+                <div className={styles.footer_center_children}>
+                  {index.dulieu.map((a, b) => {
+                    return (
+                      <Col lg={6} md={11} xs={24} key={b}>
+                        <div className={styles.footer_center_icon_children}>
+                          <i className={a.icon}></i> <span>{a.title}</span>
+                        </div>
+                      </Col>
+                    )
+                  })}
+                </div>
               </Col>
             )
           })}
+        </Row>
+      </div>
+      <div className={styles.footer_bottom}>
+        <Row>
+          <Col lg={12} md={12} xs={24} className={styles.footer_bottom_Col_1}>Copyright ® 2020 EngStar - All Rights Reserved</Col>
+          <Col lg={12} md={12} xs={24} className={styles.footer_bottom_Col_2}>
+            <Col lg={5} md={10} className={styles.footer_bottom_Col_2_v1}>
+                <div className={styles.footer_bottom_Col_2_v1_circle}>
+                  <i className="bi bi-google"></i>
+                </div>
+                <div className={styles.footer_bottom_Col_2_v1_circle}>
+                  <FontAwesomeIcon icon={faFacebookF} className={styles.icon2} />
+                </div>
+                <div className={styles.footer_bottom_Col_2_v1_circle}>
+                  <FontAwesomeIcon icon={faLinkedinIn} className={styles.icon2} />
+                </div>
+            </Col>
+            <Col lg={6} md={12}>
+              <div className={styles.footer_bottom_Col_2_v2}>Điều khoản & chính sách</div>
+            </Col>
+          </Col>
         </Row>
       </div>
     </div>
