3075a183c6d203e3e5ab11b43dcd141f5401c457
Duong Luc <83877114+lucduong96@users.noreply.github.com>
2022-09-07 10:00:07 +0700

    Update file changes
    Update file changes

--- [DuongTheLuc]_BaiTap1_HTMLCSSJS/ContactUs/js/index.js
+++ [DuongTheLuc]_BaiTap1_HTMLCSSJS/ContactUs/js/index.js
@@ -3,17 +3,19 @@ function ValidateEmail(inputText) {
   var i = 0;
   var inputText = document.getElementById("email").value;
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (inputText.match(mailformat)) {
-  } else {
-    // i++;
-    alert("Email is not valid!");
-  }
   var inputPhone = document.getElementById("phone").value;
   var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
-  if (inputPhone.match(phoneno)) {
-    return true;
+  if (inputText.match(mailformat)) {
   } else {
-    // i++;
+    alert("Email is not valid!");
+  }
+
+  if (inputPhone.match(phoneno)) {
+  } else {
     alert("Phone is not valid!");
+  }
+
+  if (inputText.match(mailformat) && inputPhone.match(phoneno)) {
+    alert("Success!");
   }
 }
--- [DuongTheLuc]_BaiTap1_HTMLCSSJS/LoginForm/js/index.js
+++ [DuongTheLuc]_BaiTap1_HTMLCSSJS/LoginForm/js/index.js
@@ -1,10 +1,9 @@
-function test() {
-}
+function test() {}
 function ValidateEmail(inputText) {
   var inputText = document.getElementById("email").value;
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if (inputText.match(mailformat)) {
-    return true;
+    return alert("Success!");
   } else {
     alert("Email không đúng! Mời nhập lại");
   }
