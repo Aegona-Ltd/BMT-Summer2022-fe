43d31b6b9ddd6ed14fc06b6fec5031369e116ba5
Duong Luc <83877114+lucduong96@users.noreply.github.com>
2022-09-09 16:55:59 +0700

    Update LoginPage with Boostrap

--- [DuongTheLuc]_BaiTap1_HTMLCSSJS/LoginForm_bs4/js/index.js
+++ [DuongTheLuc]_BaiTap1_HTMLCSSJS/LoginForm_bs4/js/index.js
@@ -3,9 +3,16 @@ function test() {
 }
 function ValidateEmail(inputText) {
   var inputText = document.getElementById("exampleDropdownFormEmail1").value;
+  var inputPassword = document.getElementById(
+    "exampleDropdownFormPassword1"
+  ).value;
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (inputText.match(mailformat)) {
+  if (inputText.match(mailformat) && inputPassword !== "") {
     return alert("Success!");
+  } else if (inputText === "") {
+    alert("Please enter email!");
+  } else if (inputPassword === "") {
+    alert("Please enter password!");
   } else {
     alert("Email không đúng! Mời nhập lại");
   }
