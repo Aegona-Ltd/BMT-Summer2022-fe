a9e594cd6daa0533ef729b91a4da888e55a963a5
thienba <macm3pro@Thien-Ba.local>
2024-02-28 11:58:09 +0700

    chore(tailwind_config): remove background image & add colors variable

--- huynhthienba_reactjs_baitap2/tailwind.config.ts
+++ huynhthienba_reactjs_baitap2/tailwind.config.ts
@@ -2,16 +2,35 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        light: {
+          100: "#FFFFFF",
+          200: "#FBFBFB",
+        },
+        dark: {
+          100: "#000000",
+          200: "#1F1F1F",
+          300: "#1B1B1B",
+          400: "#131313",
+          500: "#1A1A1A",
+          600: "#1E1E1E",
+        },
+        grey: {
+          100: "#8D8D8D",
+          200: "#E0E0E0",
+          300: "#484848",
+          400: "#616161",
+          500: "#717171",
+          600: "#848484",
+          700: "#C9C9C9",
+        },
       },
     },
   },
